408c641494b287a1fd2a8c3662a2db5685c4aaf4
Alex Pravda <kabinkos@gmail.com>
2020-11-19 11:37:30 +0200

    added 3 Hello world via System.out.println

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(HelloWorldMethod.returnHello());
-
+        System.out.println("Hello World #1");
+        System.out.println("Hello World #2");
+        System.out.println("Hello World #3");
     }
 }
